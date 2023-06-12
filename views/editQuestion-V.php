af69577aa4fef2d0d5a830071f4ec57d175665d2
felixsymanzik <128364657+felixsymanzik@users.noreply.github.com>
2023-06-12 19:58:21 +0200

    Design Anpassungen

--- views/editQuestion-V.php
+++ views/editQuestion-V.php
@@ -17,12 +17,10 @@
                 
             
         </header>
-
-        
-                <h2>Frage bearbeiten</h2>
-
-        
-    
+        <section class="previewHeadline">
+            Frage bearbeiten
+        </section>
+        <br>
     <form action="../classes/editQuestion-C.php" method="post">
         
         
@@ -32,28 +30,33 @@
 
                     
 
-                    
-                    <textarea readonly class="flexContainerEQQuestionItemQuestion" name="id" id="id" >
+                    <label class="previewHeadline" for="name">ID:</label>
+                    <textarea class="insertForm" readonly class="flexContainerEQQuestionItemQuestion" name="id" id="id" >
                     <?php echo $id; ?>
                     </textarea>
 
-                    <textarea class="flexContainerEQQuestionItemQuestion" name="frage" id="frage" >
+                    <label class="previewHeadline" for="frage">Frage:</label>
+                    <textarea class="insertForm" class="flexContainerEQQuestionItemQuestion" name="frage" id="frage" >
                     <?php echo $data['frage']; ?>
                     </textarea>
 
-                    <textarea class="flexContainerEQQuestionItemQuestion" name="antwort1_richtig" id="antwort1_richtig" >
+                    <label class="previewHeadline" for="antwort1_richtig">Richtige Antwort:</label>
+                    <textarea class="insertForm" class="flexContainerEQQuestionItemQuestion" name="antwort1_richtig" id="antwort1_richtig" >
                     <?php echo $data['antwort1_richtig']; ?>
                     </textarea>
 
-                    <textarea class="flexContainerEQQuestionItemQuestion" name="antwort2" id="antwort2" >
+                    <label class="previewHeadline" for="antwort2">Erste falsche Antwort:</label>
+                    <textarea class="insertForm" class="flexContainerEQQuestionItemQuestion" name="antwort2" id="antwort2" >
                     <?php echo $data['antwort2']; ?>
                     </textarea>
 
-                    <textarea class="flexContainerEQQuestionItemQuestion" name="antwort3" id="antwort3" >
+                    <label class="previewHeadline" for="antwort3">Zweite falsche Antwort:</label>
+                    <textarea class="insertForm" class="flexContainerEQQuestionItemQuestion" name="antwort3" id="antwort3" >
                     <?php echo $data['antwort3']; ?>
                     </textarea>
 
-                    <textarea  class="flexContainerEQQuestionItemQuestion" name="antwort4" id="antwort4" >
+                    <label class="previewHeadline" for="antwort4">Dritte falsche Antwort:</label>
+                    <textarea class="insertForm" class="flexContainerEQQuestionItemQuestion" name="antwort4" id="antwort4" >
                     <?php echo $data['antwort4']; ?>
                     </textarea>
 
@@ -68,24 +71,21 @@
     
         
 
-        <div class="flexContainerEQQuestionItemButton">
-            <button>Frage speichern</button>
-        </div>
+       
         <br><br>
     </form>
 
     <form action="../classes/editQuestion-C.php" method="post">
         <input value="delete" name="goal" type="hidden" >
         <input value=<?php echo "$id" ?> name="id" type="hidden" >
-        <button>Frage löschen</button>
+        <button class="deleteButton">Frage löschen</button>
         </form>
 
     <section id="arrowBox">
             <a href="../classes/editQuestionChoice-C.php?goal=id" ><img src="/img/arrowLeft.png" alt="" class="arrow"></a>
-            <!-- Damit der Zurück-Pfeil auch ohne Weiter-Pfeil
-            immer an der gleichen Stelle ist habe ich einen leeren
-            Platzhalter eingefügt. JFL-->
-            <div class="arrowPlaceholder"></div> 
+            <div class="flexContainerEQQuestionItemButton">
+                <button class="submButton">Frage speichern</button>
+            </div>
         </section>
 
 </body>
