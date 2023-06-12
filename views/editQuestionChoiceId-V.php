e9d199be2f381a3c997c80e60ad39e96a92626d5
felixsymanzik <128364657+felixsymanzik@users.noreply.github.com>
2023-06-12 19:41:19 +0200

    Anpassung Dropdown und Button

--- views/editQuestionChoiceId-V.php
+++ views/editQuestionChoiceId-V.php
@@ -22,34 +22,18 @@
             
         </header>
 
-        
-        <h2>Bitte wähle eine ID aus</h2>
-
-        
-    
-    
-        
+        <section class="previewHeadline">
+            Bitte wähle die ID der Frage, die du bearbeiten möchtest:
+        </section>
         
         <div >
             
             
-            <select id="idSelect" >
+            <select class="selectDropdown" id="idSelect" >
                 <?php foreach($ids as $datai) { ?>
                     <option name="id" id="id" value="<?php echo $datai['id']; ?>"><?php echo $datai['id']; ?></option>
                 <?php } ?>
             </select>
-           
-                    
-
-        </div>
-
-
-        
-
-
-        
-        <div class="flexContainerEQQuestionItemButton">
-            <button onClick="sendid()">Frage bearbeiten</button>
         </div>
         <br><br>
         
@@ -57,10 +41,9 @@
 
     <section id="arrowBox">
             <a href="../classes/editQuestionChoice-C.php?goal=modul" ><img src="../img/arrowLeft.png" alt="" class="arrow"></a>
-            <!-- Damit der Zurück-Pfeil auch ohne Weiter-Pfeil
-            immer an der gleichen Stelle ist habe ich einen leeren
-            Platzhalter eingefügt. JFL-->
-            <div class="arrowPlaceholder"></div> 
+            <div class="flexContainerEQQuestionItemButton">
+                <button class="navigationButton" onClick="sendid()">Frage bearbeiten</button>
+            </div> 
         </section>
 
 </body>
