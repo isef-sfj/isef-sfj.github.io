41d0bf3090e7b1240747737f4f122ef0c58e34ce
felixsymanzik <128364657+felixsymanzik@users.noreply.github.com>
2023-06-12 05:44:13 +0200

    editierung Textfelder

--- views/addQuestion-V.php
+++ views/addQuestion-V.php
@@ -49,19 +49,19 @@
         <div class="flexContainerEQQuestion">
 
             <label class="previewHeadline" for="frage">Frage eingeben:</label>    
-            <textarea class="flexContainerEQQuestionItemQuestion" placeholder="Frage eingeben" name="frage" id="frage" required></textarea>
+            <textarea class="insertForm" class="flexContainerEQQuestionItemQuestion" placeholder="Frage eingeben" name="frage" id="frage" required></textarea>
             
             <label class="previewHeadline" for="antwort1_richtig">Richtige Antwort eingeben:</label>
-            <textarea class="flexContainerEQQuestionItemAnswer" placeholder="RICHTIGE Antwort eingeben" name="antwort1_richtig" id="antwort1_richtig" required></textarea>
+            <textarea class="insertForm" class="flexContainerEQQuestionItemAnswer" placeholder="RICHTIGE Antwort eingeben" name="antwort1_richtig" id="antwort1_richtig" required></textarea>
             
             <label class="previewHeadline" for="antwort2">Erste falsche Antwort eingeben:</label>
-            <textarea class="flexContainerEQQuestionItemAnswer" placeholder="Falsche Antwort eingeben" name="antwort2" id="antwort2" required></textarea>
+            <textarea class="insertForm" class="flexContainerEQQuestionItemAnswer" placeholder="Falsche Antwort eingeben" name="antwort2" id="antwort2" required></textarea>
             
             <label class="previewHeadline" for="antwort3">Zweite falsche Antwort eingeben:</label>
-            <textarea class="flexContainerEQQuestionItemAnswer" placeholder="Falsche Antwort eingeben" name="antwort3" id="antwort3" required></textarea>
+            <textarea class="insertForm" class="flexContainerEQQuestionItemAnswer" placeholder="Falsche Antwort eingeben" name="antwort3" id="antwort3" required></textarea>
             
             <label class="previewHeadline" for="antwort4">Dritte falsche Antwort eingeben:</label>
-            <textarea class="flexContainerEQQuestionItemAnswer" placeholder="Falsche Antwort eingeben" name="antwort4" id="antwort4" required></textarea>
+            <textarea class="insertForm" class="flexContainerEQQuestionItemAnswer" placeholder="Falsche Antwort eingeben" name="antwort4" id="antwort4" required></textarea>
             
             
 
