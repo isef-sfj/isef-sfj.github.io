6206d916cdde9e2bfc4ccc5ced7e2002ec5415da
Jochen Linux-PC <jochen.fritzel@mailbox.org>
2023-07-11 18:29:15 +0200

    Quiz-Modul angefangen, die 2.

--- classes/quiz-M.php
+++ classes/quiz-M.php
@@ -25,55 +25,82 @@ class Quiz extends Dbh {
     private $quiz_status;
  
     // Neues Quiz speichern
-    public function setQuiz($modul, $lektion, $frage, $antwort1_richtig, $antwort2, $antwort3, $antwort4) {
+    public function setQuiz($timestamp, $modul ,$lektion, $player1, $player2, $player3, $player4, $player_count, $player1_status, $player2_status,
+                            $player3_status, $player4_status, $player1_points, $player2_points, $player3_points, $player4_points, $quiz_status) {
         
+        $this->timestamp = $timestamp;
+        $this->modul = $modul;
+        $this->lektion = $lektion;
+        $this->player1 = $player1;
+        $this->player2 = $player2;
+        $this->player3 = $player3;
+        $this->player4 = $player4;
+        $this->player_count = $player_count;
+        $this->player1_status = $player1_status;
+        $this->player2_status = $player2_status;
+        $this->player3_status = $player3_status;
+        $this->player4_status = $player4_status;
+        $this->player1_points = $player1_points;
+        $this->player2_points = $player2_points;
+        $this->player3_points = $player3_points;
+        $this->player4_points = $player4_points;
+        $this->quiz_status = $quiz_status;
         
-    $this->id = $id;
-    $this->timestamp = $timestamp;
-    $this->modul = $modul;
-    $this->lektion = $lektion;
-    $this->player1 = $player1;
-    $this->player2 = $player2;
-    $this->player3 = $player3;
-    $this->player4 = $player4;
-    $this->player_count = $player_count;
-    $this->player1_status = $player1_status;
-    $this->player2_status = $player2_status;
-    $this->player3_status = $player3_status;
-    $this->player4_status = $player4_status;
-    $this->player1_points = $player1_points;
-    $this->player2_points = $player2_points;
-    $this->player3_points = $player3_points;
-    $this->player4_points = $player4_points;
-    $this->quiz_status = $quiz_status;
-        
-        $sql = "INSERT INTO quizfragen (modul,
-                                        lektion,
-                                        frage,
-                                        antwort1_richtig,
-                                        antwort2,
-                                        antwort3,
-                                        antwort4)
-                                VALUES (:modul,
-                                        :lektion,
-                                        :frage,
-                                        :antwort1_richtig,
-                                        :antwort2,
-                                        :antwort3,
-                                        :antwort4)";
+        $sql = "INSERT INTO quiz    (modul,
+                                    lektion,
+                                    player1,
+                                    player2,
+                                    player3,
+                                    player4,
+                                    player_count,
+                                    player1_status,
+                                    player2_status,
+                                    player3_status,
+                                    player4_status,
+                                    player1_points,
+                                    player2_points,
+                                    player3_points,
+                                    player4_points,
+                                    quiz_status)
+                            VALUES (:modul,
+                                    :lektion,
+                                    :player1,
+                                    :player2,
+                                    :player3,
+                                    :player4,
+                                    :player_count,
+                                    :player1_status,
+                                    :player2_status,
+                                    :player3_status,
+                                    :player4_status,
+                                    :player1_points,
+                                    :player2_points,
+                                    :player3_points,
+                                    :player4_points,
+                                    :quiz_status)";
 
         $stmt = $this->connect()->prepare($sql);
 
-        $stmt->execute(['modul' => $this->modul,
+        $stmt->execute(['timestamp' => $this->timestamp,
+                        'modul' => $this->modul,
                         'lektion' => $this->lektion,
-                        'frage' => $this->frage,
-                        'antwort1_richtig' => $this->antwort1_richtig,
-                        'antwort2' => $this->antwort2,
-                        'antwort3' => $this->antwort3,
-                        'antwort4' => $this->antwort4]);
+                        'player1' => $this->player1,
+                        'player2' => $this->player2,
+                        'player3' => $this->player3,
+                        'player4' => $this->player4,
+                        'player_count' => $this->player_count,
+                        'player1_status' => $this->player1_status,
+                        'player2_status' => $this->player2_status,
+                        'player3_status' => $this->player3_status,
+                        'player4_status' => $this->player4_status,
+                        'player1_points' => $this->player1_points,
+                        'player2_points' => $this->player2_points,
+                        'player3_points' => $this->player3_points,
+                        'player4_points' => $this->player4_points,
+                        'quiz_status' => $this->quiz_status]);
     }
 
-
+    /*
     // Quizfrage bearbeiten
     public function editQuestion($id, $frage, $antwort1_richtig, $antwort2, $antwort3, $antwort4) {
 
